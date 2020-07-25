9b26aad93c18718de25c4329db308f44326c049f
Courtney Nicholas <68786786+courtney-tn@users.noreply.github.com>
2020-07-25 19:49:45 +0100

    BASH scripts added
    All of them are identical except the login route URL.
    No readme/licence at this time. I've tested the btbroadband.sh one, works perfectly.

---
+++ btwifipayg.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Configuration Settings
+
+IDENTITY=''
+PASSWORD=''
+
+# Begin script
+
+# Check login status: mostly taken from https://gist.github.com/sscarduzio/05ed0b41d6234530d724 pretty much with some other mods by me
+IS_LOGGED_IN=$(wget --no-check-certificate "https://www.btwifi.com:8443/home" --timeout 30 -O - 2>/dev/null | grep "accountLogoff")
+
+if [ "$IS_LOGGED_IN" ]
+    then
+      echo 'Error: you are already logged in.'
+
+    else
+    echo 'Attempting to auto-login using provided credentials...'
+    OUT=$(wget -qO - --no-check-certificate --no-cache --post-data "username=$IDENTITY&password=$PASSWORD" "https://www.btwifi.com:8443/ante")
+
+    ONLINE=$(echo $OUT | grep "now logged on" )
+
+    if [ "$ONLINE" ]
+     then
+       echo "Successful connection made to BTWi-fi."
+       logger -t "logon_hotspot" "Successful connection to hotspot."
+     else
+       echo "Error making a connection."
+       logger -t "logon_hotspot" "Error making a connection."
+     fi
+fi
+
+# End Script
